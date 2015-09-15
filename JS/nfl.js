7c903b8570e232dc9ba7c169b8bf2cabe496bf8a
Robert Walker <demond611@gmail.com>
2015-09-15 18:09:52 -0500

    9/15 quiz app commit

--- JS/nfl.js
+++ JS/nfl.js
@@ -1,14 +1,14 @@
+// GLOBAL VARIABLES
+	var counter = 0;
+	var numberOfQuestions = 0;
+	var correctAnswers = 0;
+
 $(function (){
 	var DEBUG = true;
 	var debug = function (msg){
 		if (DEBUG == true)
 			console.log("MESSAGE:", msg);
 	};
-
-	// GLOBAL VARIABLES
-	var counter = 0;
-	var numberOfQuestions = 0;
-	var correctAnswers = 0;
 
 
 	// QUESTION OBJ
@@ -122,8 +122,7 @@ $(function (){
 
 		$("#answer-btn").on("click", function (){
 			preventDefault(event);
-			checkAnswer();
-			numberOfQuestions++;
+			checkAnswer(counter);
 
 			if (numberOfQuestions === quizQuestions.length) {
 
@@ -136,34 +135,39 @@ $(function (){
 					displayResults();
 				});
 
-			} else{
+			} else {
 
 				nextQuestion();
 			
-			}		
+			}
+
+			$("#answer-btn, #cancel-btn").show();
+			numberOfQuestions++;		
 		});
 
 		$("#cancel-btn").click(function (){
 			preventDefault(event);
-			reStartQuiz();
+			// reStartQuiz();
 		});
+
+		debug("Counter: " + counter);
+		debug("Num of Ques: " + numberOfQuestions);
 	}
 
 	function preventDefault (event){
 		return event.preventDefault();
 	}
 
-	function checkAnswer () {
+	function checkAnswer (counter) {
 		var userAns = $("[type='radio']:checked").data('quesval');
 		var actualAns = quizQuestions[counter].correctChoice;
 		if (userAns === actualAns)
 			correctAnswers++;
 		debug(correctAnswers);
-		counter++;
 	}
 
 	function nextQuestion () {
-
+		$("#questions").show();
 		$("#number").text(quizQuestions[counter].questionNum);
 		$(".totalNumber").text(quizQuestions.length);
 
@@ -178,10 +182,13 @@ $(function (){
 			var questionChoices = "<li><input type='radio' name='Q"+quizCounter+"' data-quesVal='"+index+"'> "+value+"</li>";
 			$("#question-responses").append(questionChoices);
 		});
+
+		counter++;
 	}
 
 	function displayResults () {
-		$("#welcome, #questions").hide();
+		$("#welcome").hide();
+		$("#questions").hide();
 		$("#results").show();
 
 		$("#correct").text(correctAnswers);
@@ -201,207 +208,17 @@ $(function (){
 
 		$("#results-btn").click(function (){
 			preventDefault(event);
-			reStartQuiz();
+			// reStartQuiz();
 		});
 	}
 
-	function reStartQuiz () {
-		counter = 0;
-		numberOfQuestions = 0;
-		correctAnswers = 0;
-		$("#question-responses").text("");
-		$("#results, #questions").hide();
-		$("#welcome").show();
-	}
+	// function reStartQuiz () {
+	// 	counter = 0;
+	// 	numberOfQuestions = 0;
+	// 	correctAnswers = 0;
+	// 	$("#question-responses").text("");
+	// 	$("#results, #questions").hide();
+	// 	$("#welcome").show();
+	// }
 
 });
-
-
-// $(function (){
-// 	var DEBUG = true;
-// 	var debug = function (msg){
-// 		if (DEBUG == true)
-// 			console.log("MESSAGE:", msg);
-// 	};
-// 	var counter = 0;
-// 	var correctUserGuess = 0;
-// 	var userResponses = [];
-// 	var answerChoices = "";
-
-// 	function Question (question, choices){
-// 		 // Initialize the instance properties​
-// 		 this.question = question;
-// 		 this.choices = choices;
-// 	}
-
-// 	// Store quiz questions in an array to access
-// 	var quizQuestions =[
-// 		new Question( "Which quarterback led all quarterbacks in fantasy points in 2014?",
-// 			[{answer:"Aaron Rodgers", correctAnswer:false},
-// 			{answer:"Andrew Luck", correctAnswer:true},
-// 			{answer:"Cam Newton", correctAnswer:false},
-// 			{answer:"Tomo Romo", correctAnswer:false}] ),
-
-// 		new Question( "Which running back led the league in rushing yards and touchdowns in 2014?",
-// 			[{answer:"Eddie Lacy", correctAnswer:false},
-// 			{answer:"DeMarco Murray", correctAnswer:true},
-// 			{answer:"Marshawn Lynch", correctAnswer:false},
-// 			{answer:"Matt Forte", correctAnswer:false}] ),
-
-// 		new Question( "Which defense led the league in safeties in 2014?",
-// 			[{answer:"Detroit Lions", correctAnswer:false},
-// 			{answer:"Carolina Panthers", correctAnswer:false},
-// 			{answer:"Green Bay Packers", correctAnswer:true},
-// 			{answer:"Arizona Cardinals", correctAnswer:false}] ),
-
-// 		new Question( "How many kicker(s) scored at least 150 total pts in 2014?",
-// 			[{answer:"1", correctAnswer:false},
-// 			{answer:"2", correctAnswer:true},
-// 			{answer:"3", correctAnswer:true},
-// 			{answer:"4", correctAnswer:false}] ),
-
-// 		new Question( "Which quarterback led the league in interceptions in 2014?",
-// 			[{answer:"Andy Dalton", correctAnswer:false},
-// 			{answer:"Eli Manning", correctAnswer:false},
-// 			{answer:"Jay Cutler", correctAnswer:true},
-// 			{answer:"Robert Griffin III", correctAnswer:false}] ),
-
-// 		new Question( "Which Defensive End led the league in sacks in 2014?",
-// 			[{answer:"Jason Pierre-Paul", correctAnswer:false},
-// 			{answer:"J.J. Watt", correctAnswer:true},
-// 			{answer:"Ziggy Ansah", correctAnswer:false},
-// 			{answer:"DeMarcus Ware", correctAnswer:false}] ),
-
-// 		new Question( "Which defense led the league in kickoff return touchdowns in 2014?",
-// 			[{answer:"Washington Redskins", correctAnswer:false},
-// 			{answer:"Jacksonville Jaguars", correctAnswer:false},
-// 			{answer:"New Orleans Saints", correctAnswer:false},
-// 			{answer:"Philadelphia Eagles", correctAnswer:true}] ),
-
-// 		new Question( "Which quarterback rushed for over 500 yards and threw for over 3200 yards in 2014?",
-// 			[{answer:"Colin Kaepernick", correctAnswer:true},
-// 			{answer:"Russell Wilson", correctAnswer:false},
-// 			{answer:"Andrew Luck", correctAnswer:false},
-// 			{answer:"Cam Newton", correctAnswer:false}] ),
-
-// 		new Question( "How many sacks did linebacker Justin Houston record in 2014 almost breaking Michael Strahan's single season record?",
-// 			[{answer:"20", correctAnswer:false},
-// 			{answer:"25", correctAnswer:false},
-// 			{answer:"18", correctAnswer:false},
-// 			{answer:"22", correctAnswer:true}] ),
-
-// 		new Question( "How many tight ends had at least 6 touchdowns and at least 80 receptions in 2014?",
-// 			[{answer:"2", correctAnswer:true},
-// 			{answer:"4", correctAnswer:false},
-// 			{answer:"6", correctAnswer:false},
-// 			{answer:"7", correctAnswer:false}] )
-// 	];
-
-// 	// 1st Instance of Questions
-// 	$("#number").text(counter+1);
-// 	// Display the initial question
-// 	$("#fantasy-question").text(quizQuestions[counter].question);
-// 	// Get the initial question choices
-// 	for (var choice in quizQuestions[counter].choices){
-// 		answerChoices = "<li><input type='radio' name='Q"+(counter+1)+"' value='" + quizQuestions[counter].choices[choice].answer + "' data-correct='" + quizQuestions[counter].choices[choice].correctAnswer + "'> "
-// 			+ quizQuestions[counter].choices[choice].answer + " </li>";
-// 		$("#question-responses").append(answerChoices);
-// 	}
-
-// 	$("[name='Q"+(counter+1)+"']").click(function (){
-// 		var th = $(this);
-// 		if (":checked"){
-// 			if (th.data('correct') == true){
-// 				correctUserGuess++;
-// 			}
-// 		}
-// 		debug(correctUserGuess);
-// 	});
-
-
-// 	// $("input").change(function (){
-// 	// 	userResponses.push($(this).data("correct"));
-// 	// 	debug(userResponses);
-// 	// 	debug("hey");
-// 	// });
-
-
-// 	function preventDefault (event){
-// 		return event.preventDefault();
-// 	}
-
-// 	$("#questions, #results, #finish-btn").hide();
-
-// 	// Loop through each question/choice set
-// 	$("#answer-btn").on("click", function (event){
-
-// 		preventDefault(event);
-
-// 		counter++;
-// 		$("#number").text(counter+1);
-// 		if (counter < quizQuestions.length){
-
-// 			$("#fantasy-question").text(quizQuestions[counter].question);
-
-// 			$("#question-responses").text("");
-
-// 			for (var choice in quizQuestions[counter].choices){
-
-// 				answerChoices = "<li><input type='radio' name='Q"+(counter+1)+"' value='" + quizQuestions[counter].choices[choice].answer + "' data-correct='" + quizQuestions[counter].choices[choice].correctAnswer + "'> "
-// 					+ quizQuestions[counter].choices[choice].answer + " </li>";
-
-// 				$("#question-responses").append(answerChoices);
-// 			}
-// 		}
-
-// 		// $("input").on("change", function (){
-// 		// 	userResponses.push($(this).data("correct"));
-// 		// 	debug(userResponses);
-// 		// });
-
-// 		// $("[name='Q"+(counter+1)+"']").click(function (){
-// 		// 	var th = $(this);
-// 		// 	if (":checked"){
-// 		// 		if (th.data('correct') == true){
-// 		// 			$("#answer-btn").click(function (){
-// 		// 				correctUserGuess++;
-// 		// 			});
-// 		// 		}
-// 		// 	}
-// 		// 	debug(correctUserGuess);
-// 		// });
-
-// 		if (counter == quizQuestions.length - 1){
-// 			$("#finish-btn").show();
-// 			$("#cancel-btn, #answer-btn").hide();
-// 		}
-
-// 	});
-
-// 	$("#welcome-btn").click(function (event){
-// 		preventDefault(event);
-// 		$("#welcome, #results").hide();
-// 		$("#questions").show();
-// 	});
-
-// 	$("#finish-btn").click(function (event){
-// 		preventDefault(event);
-// 		$("#questions, #welcome").hide();
-// 		$("#results").show();
-
-// 		// debug(userResponses);
-// 	});
-
-// 	$("#results-btn").click(function (event){
-// 		preventDefault(event);
-// 		$("#questions, #results").hide();
-// 		$("#welcome").show();
-// 	});
-
-// 	$("#cancel-btn").click(function (event){
-// 		preventDefault(event);
-// 		$("#welcome").show();
-// 		$("#questions, #results").hide();
-// 	});
-
-// });
